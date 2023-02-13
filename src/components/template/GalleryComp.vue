9e3f2fbadbc9bd82f90e0e46721d48787bfbbaae
FIRQY <firqy.sutan@gmail.com>
2023-02-13 14:05:06 +0700

    Latest Update

--- src/components/template/GalleryComp.vue
+++ src/components/template/GalleryComp.vue
@@ -167,6 +167,78 @@
             @hide="handleHide"
           ></vue-easy-lightbox>
         </div>
+
+        <div class="page-box">
+          <ul class="pagination" role="menubar" aria-label="Pagination">
+            <li
+              v-for="link in pagination.links"
+              :key="link.id"
+              :class="paginationClass(link.url, link.active)"
+            >
+              <a
+                v-if="link.label.includes('Previous')"
+                @click="paginationClick(link.url)"
+                class="pagination-tab"
+              >
+                <span
+                  :style="
+                    link.url != null
+                      ? { color: 'lightseagreen' }
+                      : { color: 'white' }
+                  "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-chevron-left"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                    />
+                  </svg>
+                </span>
+              </a>
+              <a
+                v-else-if="link.label.includes('Next')"
+                @click="paginationClick(link.url)"
+                class="pagination-tab"
+              >
+                <span
+                  :style="
+                    link.url != null
+                      ? { color: 'lightseagreen' }
+                      : { color: 'white' }
+                  "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-chevron-right"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                    />
+                  </svg>
+                </span>
+              </a>
+              <a
+                v-else
+                @click="paginationClick(link.url)"
+                class="pagination-tab"
+              >
+                {{ link.label }}
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
